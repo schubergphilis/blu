34fdaf101668b18311b6d4a79722b92da8ff4722
Anooshiravan Ahmadi <aahmadi@schubergphilis.com>
2017-11-09 17:56:03 +0100

    Initial structure for BluSnapin and Invoke-Chefrequest cmdlet

---
+++ BluSnapin/Cmdlet/Invoke_ChefRequest/Invoke_ChefRequest_Params.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace BluSnapin.Cmdlet
+{
+    public partial class Invoke_ChefRequest : PSCmdlet
+    {
+    }
+}
